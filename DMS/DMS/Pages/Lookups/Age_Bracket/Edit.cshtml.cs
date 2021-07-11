ff519fa3d638bfbb0d4e2589bdca5e33f883451a
YGeller <yevgeller+git@gmail.com>
2021-07-11 10:16:32 -0500

    Added Age_Bracket lookup

---
+++ DMS/DMS/Pages/Lookups/Age_Bracket/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DMS.Data;
+using DMS.Models;
+
+namespace DMS.Pages.Lookups.Age_Bracket
+{
+    public class EditModel : PageModel
+    {
+        private readonly DMS.Data.DMSDataContext _context;
+
+        public EditModel(DMS.Data.DMSDataContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Models.Age_Bracket Age_Bracket { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Age_Bracket = await _context.Age_Bracket.FirstOrDefaultAsync(m => m.Age_Bracket_Id == id);
+
+            if (Age_Bracket == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Age_Bracket).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!Age_BracketExists(Age_Bracket.Age_Bracket_Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool Age_BracketExists(int id)
+        {
+            return _context.Age_Bracket.Any(e => e.Age_Bracket_Id == id);
+        }
+    }
+}
