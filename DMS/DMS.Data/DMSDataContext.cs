71f751cd4cb29e3df114b68096f940e5d4381613
YGeller <yevgeller+git@gmail.com>
2021-07-11 10:09:08 -0500

    Adding db, first migration

--- DMS/DMS.Data/DMSDataContext.cs
+++ DMS/DMS.Data/DMSDataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DMS.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +8,11 @@ namespace DMS.Data
 {
     public class DMSDataContext : DbContext
     {
+        public DbSet<Age_Bracket> Age_Bracket { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("Data Source=MyDb.db", b=>b.MigrationsAssembly("DMS"));
+        }
     }
 }
---
+++ DMS/DMS/Migrations/20210711150821_firsttest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DMS.Migrations
+{
+    public partial class firsttest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Age_Bracket",
+                columns: table => new
+                {
+                    Age_Bracket_Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Age_Bracket", x => x.Age_Bracket_Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Age_Bracket");
+        }
+    }
+}
