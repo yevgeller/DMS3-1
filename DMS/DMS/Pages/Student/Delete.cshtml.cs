12b739a5c731dd1a7d6f1c459cb128a751a3154e
YGeller <yevgeller+git@gmail.com>
2021-07-13 12:13:56 -0500

    Adding Student CRUD pages

---
+++ DMS/DMS/Pages/Student/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DMS.Data;
+using DMS.Models;
+
+namespace DMS.Pages.Student
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly DMS.Data.DMSDataContext _context;
+
+        public DeleteModel(DMS.Data.DMSDataContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Models.Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Student.FirstOrDefaultAsync(m => m.Student_Id == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Student.FindAsync(id);
+
+            if (Student != null)
+            {
+                _context.Student.Remove(Student);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
