ec0f11dec696a1c39d8b82495971e7e0fca9ebc3
YGeller <yevgeller+git@gmail.com>
2021-07-12 21:54:05 -0500

    Added Person and Room editing pages

---
+++ DMS/DMS/Pages/Person/Delete.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DMS.Data;
+using DMS.Models;
+
+namespace DMS.Pages.Person
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly DMS.Data.DMSDataContext _context;
+
+        public DeleteModel(DMS.Data.DMSDataContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Models.Person Person { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Person = await _context.Person
+                .Include(p => p.Person_Type).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Person == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Person = await _context.Person.FindAsync(id);
+
+            if (Person != null)
+            {
+                _context.Person.Remove(Person);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ DMS/DMS/Pages/Person/Edit.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DMS.Data;
+using DMS.Models;
+
+namespace DMS.Pages.Person
+{
+    public class EditModel : PageModel
+    {
+        private readonly DMS.Data.DMSDataContext _context;
+
+        public EditModel(DMS.Data.DMSDataContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Models.Person Person { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Person = await _context.Person
+                .Include(p => p.Person_Type).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Person == null)
+            {
+                return NotFound();
+            }
+           ViewData["Person_Type_Id"] = new SelectList(_context.Person_Type, "Person_Type_Id", "Name");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Person).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonExists(Person.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool PersonExists(int id)
+        {
+            return _context.Person.Any(e => e.Id == id);
+        }
+    }
+}
