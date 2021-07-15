7c9af6d94fdbf932a543c98fc11404c4f0519ac4
YGeller <yevgeller+git@gmail.com>
2021-07-14 21:25:31 -0500

    Moved Password to Person, removed old scaffolded pages.

---
+++ DMS/DMS/Migrations/20210715021832_MovingPasswordFromContactToPerson.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DMS.Migrations
+{
+    public partial class MovingPasswordFromContactToPerson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Contact");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Person",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Person");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Contact",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
