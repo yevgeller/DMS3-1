37b7291feb56b6828bf912442f56f9066e2c98cb
YGeller <yevgeller+git@gmail.com>
2021-07-14 21:39:06 -0500

    Re-scaffolded entity pages

---
+++ DMS/DMS/Pages/Room/Delete.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DMS.Data;
+using DMS.Models;
+
+namespace DMS.Pages.Room
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly DMS.Data.DMSDataContext _context;
+
+        public DeleteModel(DMS.Data.DMSDataContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Models.Room Room { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Room = await _context.Room
+                .Include(r => r.Age_Bracket).FirstOrDefaultAsync(m => m.Room_Id == id);
+
+            if (Room == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Room = await _context.Room.FindAsync(id);
+
+            if (Room != null)
+            {
+                _context.Room.Remove(Room);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
