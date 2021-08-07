d35f065eba8f259ab15094453b242b341c8e3a99
YGeller <yevgeller+git@gmail.com>
2021-08-06 23:01:12 -0500

    Student list is feature complete

---
+++ DMS/DMS/Models/PaginatedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DMS.Models
+{
+    public class PaginatedList<T> : List<T>
+    {
+        //https://docs.microsoft.com/en-us/aspnet/core/data/ef-rp/sort-filter-page?view=aspnetcore-5.0
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+        public int TotalRecordsCount { get; private set; }
+
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            TotalRecordsCount = count;
+
+            this.AddRange(items);
+        }
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => (PageIndex < TotalPages);
+        public static async Task<PaginatedList<T>> CreateAsync(
+            IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip(
+                (pageIndex - 1) * pageSize)
+                .Take(pageSize).ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
--- DMS/DMS/Pages/Student/Index.cshtml.cs
+++ DMS/DMS/Pages/Student/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using DMS.Data;
 using DMS.Models;
 
@@ -14,50 +15,70 @@ namespace DMS.Pages.Student
     {
         private readonly DMS.Data.DMSDataContext _context;
         private readonly IStudentData studentService;
-
-        public IndexModel(DMS.Data.DMSDataContext context, IStudentData _studentService)
+        private readonly IConfiguration Configuration;
+        public IndexModel(DMS.Data.DMSDataContext context, IStudentData _studentService, IConfiguration _configuration)
         {
             _context = context;
             studentService = _studentService;
+            Configuration = _configuration;
         }
 
-        [BindProperty(SupportsGet = true)]
-        public int CurrentPage { get; set; } = 1;
-        
-        [BindProperty(SupportsGet = true)]
-        public string SortBy { get; set; }
+        //[BindProperty(SupportsGet = true)]
+        //public int CurrentPage { get; set; } = 1;
+
+        //[BindProperty(SupportsGet = true)]
+        //public string SortBy { get; set; }
 
         public string NameSort { get; set; }
         public string RoomSort { get; set; }
+        public string BirthdateSort { get; set; }
+        public string IsActiveSort { get; set; }
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
         public int Count { get; set; }
 
+        public string PagerInfo
+        {
+            get
+            {
+                return $"Page {Students_List.PageIndex} of {TotalPages} ({this.Count} record{(this.Count > 1 ? "s" : "")})";
+            }
+        }
+
         [BindProperty(SupportsGet = true)]
         public int PageSize { get; set; }
         public int TotalPages => (int)Math.Ceiling(decimal.Divide(Count, PageSize));
-        public List<Models.Person> Data { get; set; }
-        public bool ShowPrevious => CurrentPage > 1;
-        public bool ShowNext => CurrentPage < TotalPages;
-        public bool ShowFirst => CurrentPage != 1;
-        public bool ShowLast => CurrentPage != TotalPages;
-
-        public IList<Models.Students_List> Students_List { get; set; }
-
-        public async Task OnGetAsync(string sortOrder, string searchString)
+        //public List<Models.Person> Data { get; set; }
+        //public bool ShowPrevious => CurrentPage > 1;
+        //public bool ShowNext => CurrentPage < TotalPages;
+        //public bool ShowFirst => CurrentPage != 1;
+        //public bool ShowLast => CurrentPage != TotalPages;
+        public PaginatedList<Students_List> Students_List { get; set; }
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
-            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            CurrentSort = sortOrder;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "Name";
             RoomSort = sortOrder == "Room" ? "room_desc" : "Room";
+            BirthdateSort = sortOrder == "Birthdate" ? "birthdate_desc" : "Birthdate";
+            IsActiveSort = sortOrder == "Is_Active" ? "isActive_desc" : "Is_Active";
 
-            //this.Count = await _context.Students_List.CountAsync();
             IQueryable<Students_List> list = from s in _context.Students_List
                                              select s;
 
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
             CurrentFilter = searchString;
-            
+
             if (!String.IsNullOrEmpty(searchString))
             {
-                list = list.Where(s => s.Student_Name.ToLower().Contains(searchString.ToLower()) || 
+                list = list.Where(s => s.Student_Name.ToLower().Contains(searchString.ToLower()) ||
                                         s.AssignedToRooms.ToLower().Contains(searchString.ToLower()));
             }
 
@@ -72,20 +93,28 @@ namespace DMS.Pages.Student
                 case "rooms_desc":
                     list = list.OrderByDescending(s => s.AssignedToRooms);
                     break;
+                case "Birthdate":
+                    list = list.OrderBy(s => s.Birthdate);
+                    break;
+                case "birthdate_desc":
+                    list = list.OrderByDescending(s => s.Birthdate);
+                    break;
+                case "Is_Active":
+                    list = list.OrderBy(s => s.Is_Active);
+                    break;
+                case "isActive_desc":
+                    list = list.OrderByDescending(s => s.Is_Active);
+                    break;
+                case "Name":
                 default:
                     list = list.OrderBy(s => s.Student_Name);
                     break;
             }
 
-            Students_List = await list
-                .AsNoTracking()
-                .Skip((CurrentPage -1) * PageSize)
-                .Take(PageSize)
-                .ToListAsync();
+            Students_List = await PaginatedList<Students_List>.CreateAsync(
+                list.AsNoTracking(), pageIndex ?? 1, PageSize);
 
-            this.Count = await list.CountAsync();
-            
-            //Students_List = await studentService.GetPaginatedStudentListAsync(CurrentPage, PageSize, SortBy);
+            this.Count = Students_List.TotalRecordsCount;
         }
     }
 }
