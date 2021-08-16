5abb2fd6525bc9204cfd66d7b1cf9f80c96b1cbc
YGeller <yevgeller+git@gmail.com>
2021-08-15 21:48:19 -0500

    Sorting Persons list, now making it get its source from Persons_List

--- DMS/DMS/Pages/Person/Index.cshtml.cs
+++ DMS/DMS/Pages/Person/Index.cshtml.cs
@@ -21,11 +21,72 @@ namespace DMS.Pages.Person
 
         public IList<Models.Person> Person { get;set; }
         public IList<Models.Persons_List> Persons_List { get; set; }
-        public async Task OnGetAsync()
+        public string SortByName { get; set; }
+        public string SortByActive { get; set; }
+        public string SortByPersonType { get; set; }
+        public string SortByAdditionalData { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+        public int PageSize { get; set; } = 200;
+        
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
-            Person = await _context.Person
-                .Include(p => p.Person_Type).ToListAsync();
-            Persons_List = await _context.Persons_List.ToListAsync();
+            CurrentSort = sortOrder;
+            SortByName = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "Name";
+            SortByActive = sortOrder == "Is_Active" ? "is_active_desc" : "Is_Active";
+            SortByPersonType = sortOrder == "Type" ? "type_desc" : "Type";
+            SortByAdditionalData = sortOrder == "data" ? "data_desc" : "data";
+
+            IQueryable<Persons_List> list = from p in _context.Persons_List select p;
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            CurrentFilter = searchString;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                list = list.Where(s => s.Name.ToLower().Contains(searchString.ToLower()) ||
+                                        s.Person_Qualifier.ToLower().Contains(searchString.ToLower()));
+            }
+
+            switch(sortOrder)
+            {
+                case "name_desc":
+                    list = list.OrderByDescending(p => p.Name);
+                    break;
+                case "Is_Active":
+                    list = list.OrderByDescending(p => p.Is_Active); //yes before no
+                    break;
+                case "is_active_desc":
+                    list = list.OrderBy(p => p.Is_Active);
+                    break;
+                case "Type":
+                    list = list.OrderBy(p => p.Person_Type_Name);
+                    break;
+                case "type_desc":
+                    list = list.OrderByDescending(p => p.Person_Type_Name);
+                    break;
+                case "data":
+                    list = list.OrderBy(p => p.Person_Qualifier);
+                    break;
+                case "data_desc":
+                    list = list.OrderByDescending(p => p.Person_Qualifier);
+                    break;
+                case "Name":
+                default:
+                    list = list.OrderBy(p => p.Name);
+                    break;
+            }
+            //Person = await _context.Person
+            //    .Include(p => p.Person_Type).ToListAsync();
+            Persons_List = await PaginatedList<Persons_List>
+                .CreateAsync(list.AsNoTracking(), pageIndex ?? 1, PageSize);
+            
         }
     }
 }
