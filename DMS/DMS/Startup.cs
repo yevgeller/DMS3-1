b696cc0a06a7f18eedafe02561cd1574bb138c6e
yevgeller <yevgeller@gmail.com>
2021-09-05 22:19:23 -0500

    Added runtime compilation,

--- DMS/DMS/Startup.cs
+++ DMS/DMS/Startup.cs
@@ -31,7 +31,7 @@ namespace DMS
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
             services.AddScoped<IStudentData, StudentService>();
             services.AddEntityFrameworkSqlite().AddDbContext<DMSDataContext>();
             services.AddControllers();
