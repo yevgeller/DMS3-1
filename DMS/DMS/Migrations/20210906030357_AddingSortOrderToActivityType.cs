5410592e805d665b279876fb07309cc6f148e61b
yevgeller <yevgeller@gmail.com>
2021-09-05 22:10:57 -0500

    Added Activity_Type.SortOrder to the db, edited pages to accept the order

---
+++ DMS/DMS/Migrations/20210906030357_AddingSortOrderToActivityType.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DMS.Migrations
+{
+    public partial class AddingSortOrderToActivityType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SortOrder",
+                table: "Activity_Type",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SortOrder",
+                table: "Activity_Type");
+        }
+    }
+}
