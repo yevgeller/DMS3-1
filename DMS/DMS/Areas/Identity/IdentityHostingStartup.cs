636347f39f019786cd6ad0ce8d44fa13b8e946e1
yevgeller <yevgeller@gmail.com>
2021-09-13 20:39:07 -0500

    Adding Authentication/Authorization to SQLite

---
+++ DMS/DMS/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DMS.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DMS.Areas.Identity.IdentityHostingStartup))]
+namespace DMS.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DMSContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DMSContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<DMSContext>();
+            });
+        }
+    }
+}
